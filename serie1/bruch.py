ec2465bda3167e96a86ee0de53b62bfe99e90bf8
Koopakiller <koopakiller@live.de>
2017-10-13 07:02:46 +0200

    Operator implementation in Bruch

--- serie1/bruch.py
+++ serie1/bruch.py
@@ -20,18 +20,29 @@ class Bruch:
     # Calculation Operators
 
     def __add__(self, other):
-        return Bruch(self.numerator * other.denominator + other.numerator * self.denominator,
-                     self.denominator * other.denominator)
+        if isinstance(other, Bruch):
+            return Bruch(self.numerator * other.denominator + other.numerator * self.denominator,
+                         self.denominator * other.denominator)
+        if isinstance(other, int) or isinstance(other, long):
+            return self.__add__(Bruch(other, 1))
+        raise TypeError()
 
     def __sub__(self, other):
-        return Bruch(self.numerator * other.denominator - other.numerator * self.denominator,
-                     self.denominator * other.denominator)
+        return self.__add__(-other)
 
     def __mul__(self, other):
-        return Bruch(self.numerator * other.numerator, self.denominator * other.denominator)
+        if isinstance(other, Bruch):
+            return Bruch(self.numerator * other.numerator, self.denominator * other.denominator)
+        if isinstance(other, int) or isinstance(other, long):
+            return self.__mul__(Bruch(other, 1))
+        raise TypeError()
 
     def __div__(self, other):
-        return Bruch(self.numerator * other.denominator, self.denominator * other.numerator)
+        if isinstance(other, Bruch):
+            return self.__mul__(Bruch(other.denominator, other.numerator))
+        if isinstance(other, int) or isinstance(other, long):
+            return self.__div__(Bruch(1, other))
+        raise TypeError()
 
     def __neg__(self):
         return Bruch(-self.numerator, self.denominator)
@@ -39,6 +50,13 @@ class Bruch:
     # Equality Operators
 
     def __eq__(self, other):
+        if isinstance(other, float):
+            return self.__float__() == other
+        if isinstance(other, int) or isinstance(other, long):
+            other = Bruch(other, 1)
+        if not isinstance(other, Bruch):
+            raise ValueError()
+
         a = self - other
         return a.numerator == 0 and a.denominator != 0
 
@@ -48,7 +66,26 @@ class Bruch:
     def __ne__(self, other):
         return not self.__eq__(other)
 
-    # String
+    def __lt__(self, other):
+        if isinstance(other, float):
+            return self.__float__() < other
+        if isinstance(other, int) or isinstance(other, long):
+            other = Bruch(other, 1)
+        if not isinstance(other, Bruch):
+            raise ValueError()
+
+        return self.numerator * other.denominator < other.denominator * self.denominator
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __gt__(self, other):
+        return not self.__le__(other)
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
+
+    # Type conversation
 
     def __str__(self):
         if self.numerator == 0:
@@ -58,3 +95,6 @@ class Bruch:
         if self.denominator == 1:
             return str(self.numerator)
         return str(self.numerator) + " / " + str(self.denominator)
+
+    def __float__(self):
+        return float(self.numerator) / float(self.denominator)
