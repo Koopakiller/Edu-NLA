05e00d867c993f2180b490368d6677341a0822af
Koopakiller <koopakiller@live.de>
2017-11-13 14:01:38 +0100

    Decimal type

--- serie2/summe.py
+++ serie2/summe.py
@@ -1,5 +1,5 @@
-import numpy
-
+from decimal import *
+import time
 
 class Sum:
 
@@ -8,28 +8,35 @@ class Sum:
 
     def sum_indices(self, addends):
         result = 0
-        for value in addends:
-            result += value
+        for addend in addends:
+            result += addend
         return result
 
     def sum_ordered_ascendends(self, addends):
         result = 0
-        for value in sorted(addends):
-            result += value
+        for addend in sorted(addends):
+            result += addend
         return result
 
 
 def get_harmonic_series_addends(k):
     result = []
     for i in range(1, k + 1):
-        result.append(1 / k)
+        result.append(Decimal(1) / Decimal(i))
+    return result
+
+
+def factorial(k):
+    result = Decimal(1)
+    for i in range(2, k + 1):
+        result *= k
     return result
 
 
 def get_e_taylor_series_addends(x, k):
     result = []
     for i in range(1, k + 1):
-        result.append(numpy.power(x, i) / numpy.math.factorial(i))
+        result.append((x ** Decimal(i)) / factorial(i))
     return result
 
 
@@ -37,26 +44,31 @@ def get_e_taylor_series_2_addends(x, k):
     result = []
     for i in range(1, k + 1):
         result.append((1 if i % 2 == 0 else -1)
-                      * numpy.power(x, i)
-                      / numpy.math.factorial(i))
+                      * (x ** i)
+                      / factorial(i))
     return result
 
 
 def main():
 
-    sum = Sum()
+    s = Sum()
 
     print("Partial sum of harmonic series")
 
     for k in map(lambda x: 2**x, range(1, 20)):
-        addends = get_harmonic_series_addends(k)
+        start = time.clock()
+        addends = get_harmonic_series_addends(Decimal(k))
         print("k = " + str(k))
-        r1 = sum.sum_indices(addends)
-        print("  added by indices: " + str(r1))
-        r2 = sum.sum_ordered_ascendends(addends)
-        print("  added by size:    " + str(r2))
+        r1 = s.sum_indices(addends)
+        print("   added by indices: " + str(r1))
+        r2 = s.sum_ordered_ascendends(addends)
+        print("   added by size:    " + str(r2))
+        print(" > elapsed time: " + str(time.clock() - start) + "s")
 
-    pass
+    print()
+    print("==================================================================")
+    print()
+
 
 
 if __name__ == "__main__":
