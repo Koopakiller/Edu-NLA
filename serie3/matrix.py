0c93433abb2df261a243b99d326a5dbde8a1b3e5
Koopakiller <koopakiller@live.de>
2017-12-11 14:38:28 +0100

    3.2 B B (Hilbert) Implementation

--- serie3/matrix.py
+++ serie3/matrix.py
@@ -72,23 +72,46 @@ class Matrix:
         return x
 
 
-def main(mtypes, dims, dtypes, experiment):
+def main_31b(mtypes, dims, dtypes):
     for mtype in mtypes:
         for dim in dims:
             for dtype in dtypes:
                 print("")
                 print("Experiment for mtype={0}, dim={1}, dtype={2}".format(mtype, dim, dtype))
 
-                if experiment == "3.1B":
-                    identity = numpy.identity(dim, dtype)
-                    matrix = Matrix(mtype, dim, dtype)
-                    m = identity - (matrix.matrix * matrix.inv)
+                identity = numpy.identity(dim, dtype)
+                matrix = Matrix(mtype, dim, dtype)
+                m = identity - (matrix.matrix * matrix.inv)
 
-                    try:
-                        m_inv = scipy.linalg.inv(m)
-                    except (numpy.linalg.linalg.LinAlgError, ValueError) as ex:
-                        print("Cannot calculate inverse of M: " + ex.message)
-                        continue
+                try:
+                    m_inv = scipy.linalg.inv(m)
+                except (numpy.linalg.linalg.LinAlgError, ValueError) as ex:
+                    print("Cannot calculate inverse of M: " + ex.message)
+                    continue
 
-                    condition = numpy.linalg.norm(m, ord=numpy.inf) * numpy.linalg.norm(m_inv, ord=numpy.inf)
-                    print("||M|| = {1}         || I - M M^(-1) || = {0}".format(condition, matrix.condition()))
+                condition = numpy.linalg.norm(m, ord=numpy.inf) * numpy.linalg.norm(m_inv, ord=numpy.inf)
+                print("||M|| = {1}         || I - M M^(-1) || = {0}".format(condition, matrix.condition()))
+
+
+def main_32b_hilbert(dtypes, n_iterable, i_iterable):
+    for dtype in dtypes:
+        for n in n_iterable:
+            for i_max in i_iterable:
+                if i_max > n:
+                    continue
+                matrix = Matrix("hilbert", n, dtype)
+                print("Hilbert Matrix with n={0} and type {1}".format(n, dtype))
+                result = numpy.identity(n, dtype=dtype)
+                for i in xrange(1, i_max + 1):
+                    result = result * matrix.matrix
+                    print("i = {0}, x^{0} = ".format(i))
+                    print(result)
+
+
+def main(experiment, mtypes=None, dims=None, dtypes=None, n=None, i=None):
+    if experiment == "3.1B":
+        main_31b(mtypes, dims, dtypes)
+    if experiment == "3.2B - B":
+        main_32b_hilbert(dtypes, n, i)
+    else:
+        print("Unknown experiment")
--- serie3/start.py
+++ serie3/start.py
@@ -5,11 +5,20 @@ import matrix
 
 
 def main():
+    experiment = "3.2B - B"  # allowed are "3.1B", "3.2B - B"
+
+    # The type to use for the experiments
+    dtypes = ["float16", "float32", "float64"]
+
+    # Parameters for 3.1A
     mtypes = ["hilbert", "saite"]
     dims = [5, 7, 9]
-    dtypes = ["float16", "float32", "float64"]
-    experiment = "3.1B"  # allowed are "3.1B"
-    matrix.main(mtypes, dims, dtypes, experiment)
+
+    # Parameters for 3.2 B B (Hilbert)
+    n = [5, 8]
+    i = [1, 2, 3, 4, 5, 6, 7, 8]  # only i<=n will be used
+
+    matrix.main(experiment, mtypes, dims, dtypes, n, i)
 
 
 if __name__ == "__main__":
