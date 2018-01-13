56704a8c8dbf0dfc1125adfc42e3805f81d238ee
Koopakiller <koopakiller@live.de>
2018-01-13 14:26:45 +0100

    Serie 3: Plot params

--- serie3/plot.py
+++ serie3/plot.py
@@ -6,17 +6,17 @@ import numpy
 
 
 def plot():
-    n = 5
+    n = 7
     h = 1.0 / n
 
-    r = numpy.arange(0, 3, 0.01)
-    plt.plot(r, numpy.sin(r), "-")
+    r = numpy.arange(0, 1, 0.01)
+    plt.plot(r, numpy.sin(r*2), "-")
 
     m = matrix.Matrix("saite", n, "float64")
-    b = numpy.array([1*h*h, 2*h*h, 3*h*h, 4*h*h, 5*h*h])
+    b = numpy.array([i*h for i in range(0, n)])
     x = m.solve(b)
 
-    plt.plot(b, x, "ro")
+    plt.plot(b, x / 55, "ro")
 
     plt.xlabel("x")
     plt.ylabel("y")
