e93ba26b1566c2fcead4a63e9fe3b3dc8649f480
odafaluy <ynofberlin@gmail.com>
2018-01-13 15:32:02 +0100

    Merge branch 'master' of /u/odafaluy/NLA with conflicts.

--- serie4/leastSquares_7.py
+++ serie4/leastSquares_7.py
@@ -1,6 +1,6 @@
 import numpy as np
+import numpy.linalg
 import math
-from plot import plot as plot
 
 
 def read_data(file_name):
@@ -21,7 +21,7 @@ def get_b_from_data_list(data_list):
 
 def get_a_from_data_list(data_list, d):
     e = math.e
-    return np.matrix(map(lambda pair: [e**(d * pair[0]), e**(-d * pair[0]), 1], data_list))
+    return np.matrix(map(lambda pair: [e**(d * pair[0]), e**(-d * pair[0]), 1, ], data_list))
 
 
 def get_d_list(n):
@@ -30,33 +30,18 @@ def get_d_list(n):
 
     result = []
     for k in range(0, n):
-        result.append((k, 0.1 + 0.4 * k / (n - 1)))
+        result.append(0.1 + 0.4 * k / (n - 1))
     return result
 
 
 def main():
     data_list = read_data("data.txt")
     b = get_b_from_data_list(data_list)
-    parameter_list = []
-    n = 7
-    for k, d in get_d_list(n):
-        print("k={0}; n={1}".format(k, n))
+    for d in get_d_list(3):
         a = get_a_from_data_list(data_list, d)
         q, r = np.linalg.qr(a)
-        if np.linalg.matrix_rank(r) != 3 or np.linalg.matrix_rank(q) != 3:
-            print("Rank of r or q is not 3!")
-        else:
-            p = np.dot(q.T, b)
-            x = np.dot(np.linalg.inv(r), p)
-            parameter_list.append((x.item(0), x.item(1), x.item(2), d, k, n))
-            r = np.dot(a, x) - b
-            print("Residuum r = Ax - b = ")
-            print(str(r))
-
-        print("")
-
-    plot(parameter_list, data_list)
-
+        print q
+        print r
 
 if __name__ == "__main__":
     main()
