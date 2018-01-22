8a00d8d617e0798505b0d1e7ed80fecb160128c7
Koopakiller <koopakiller@live.de>
2018-01-22 16:46:20 +0100

    fdm_cond get_matrix

---
+++ serie5/fdm_cond.py
@@ -0,0 +1,28 @@
+import scipy.sparse
+
+
+def get_matrix(n):
+    size = (n-1)*(n-1)
+    result = scipy.zeros((size, size))
+    for block in range(0, n - 1):
+        for i in range(0, n - 1):
+            result[(block * (n-1)) + i, (block * (n-1)) + i] = 4
+            if i > 0:
+                result[(block * (n-1)) + i, (block * (n-1)) + i - 1] = -1
+            if i < n-2:
+                result[(block * (n-1)) + i, (block * (n-1)) + i + 1] = -1
+            if block > 0:
+                result[(block * (n-1)) + i, ((block-1) * (n-1)) + i] = -1
+            if block < n-2:
+                result[(block * (n-1)) + i, ((block+1) * (n-1)) + i] = -1
+    return result
+
+
+def main():
+    matrix = get_matrix(5)
+    print(matrix)
+    pass
+
+
+if __name__ == "__main__":
+    main()
