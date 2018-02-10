3cccd788a1e80f80e85c7f0d673573dd05d295fb
Koopakiller <koopakiller@live.de>
2018-02-10 16:33:59 +0100

    Algorithm

--- serie5/start5.py
+++ serie5/start5.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     #  First:  "b" or "c" for the Plots in 5.3 b respective 5.3 c
     #  Second: Parameters for the plots. e.g.:
     #    "b": {"omega": 5, "eps": 10**-5, "sor_plot": [10], "exact_plot": []}
-    #    "c": {"omega": 5, "eps": }
+    #    "c": {"omega": 5, "n": [3, 4, 5, 6, 7, 8], "eps": [10**-2, 10**-4, 10**-6]}
 
-    main("b", {"omega": 5, "eps": 10**-5, "sor_plot": [10], "exact_plot": []})
+    #main("c", {"omega": 5, "n": [3, 4, 5, 6, 7, 8], "eps": [(10**-1, "ro"), (10**-2, "go"), (10**-3, "bo")]})
+    main("b", {"omega": 1, "eps": 10**-8, "sor_plot": [5, 10], "exact_plot": []})
